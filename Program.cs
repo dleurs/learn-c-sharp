9000728ad9eeda74e5799089381d1630e73ab378
Dimitri LEURS <dimitri@leurs.dev>
2025-03-13 12:50:19 +0100

    feat(1): Helloworld

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// dotnet new console -n HelloWorld
+
+// dotnet run
+
+using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
